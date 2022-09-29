f825904e2a3cb98913fc737d9f182d51dc1c86c0
Wiktoria Drezner <93138210+wiktoriadrezner@users.noreply.github.com>
2022-09-29 22:47:13 +0200

    Admin Login

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
+const expressSession = require("express-session");
 const sqlite3 = require("sqlite3");
 
+const adminUsername = "dreznerwiktoria";
+const adminPassword = "grandYearPass2022()123admin";
+
+// SET UP DATABASE
 const database = new sqlite3.Database("grand-year-database.db");
 database.run(`
 	CREATE TABLE IF NOT EXISTS contact (
-		id INTEGER PRIMARY KEY,
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		email TEXT,
 		message TEXT,
         response TEXT
 	);
 `);
-
-// CREATE TABLE IF NOT EXISTS news (
-//     id INTEGER PRIMARY KEY,
-//     date TEXT,
-//     title TEXT,
-//     post TEXT
-// );
-// CREATE TABLE IF NOT EXISTS experiences (
-//     id INTEGER PRIMARY KEY,
-//     name TEXT,
-//     link TEXT,
-//     experience TEXT
-// );
+database.run(`
+	CREATE TABLE IF NOT EXISTS news (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT,
+        title TEXT,
+        post TEXT
+	);
+`);
+database.run(`
+	CREATE TABLE IF NOT EXISTS experiences (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        link TEXT,
+        experience TEXT
+	);
+`);
 
 const app = express();
 
@@ -42,11 +50,27 @@ app.engine(
 );
 app.set("view engine", "hbs");
 
-// Routing
+// SESSION
+app.use(
+    expressSession({
+        saveUninitialized: false,
+        resave: false,
+        secret: "oiakfgnunwgklyf",
+    })
+);
+
+// ROUTING
 app.get("/", (request, response) => {
     response.render("index", {
+        session: request.session,
         title: "Grand Year",
         style: "index.css",
+    });
+});
+app.get("/admin", (request, response) => {
+    response.render("admin", {
+        title: "Admin",
+        style: "admin.css",
     });
 });
 app.get("/mission", (request, response) => {
@@ -62,17 +86,23 @@ app.get("/opportunities", (request, response) => {
     });
 });
 app.get("/news", (request, response) => {
-    response.render("news", {
-        title: "News",
-        style: "news.css",
-        news: data.news,
+    const query = `SELECT * FROM news`;
+    database.all(query, function (error, news) {
+        response.render("news", {
+            title: "News",
+            style: "news.css",
+            news: news,
+        });
     });
 });
 app.get("/experiences", (request, response) => {
-    response.render("experiences", {
-        title: "Experiences",
-        style: "experiences.css",
-        experiences: data.experiences,
+    const query = `SELECT * FROM experiences`;
+    database.all(query, function (error, experiences) {
+        response.render("experiences", {
+            title: "Experiences",
+            style: "experiences.css",
+            experiences: experiences,
+        });
     });
 });
 app.get("/contact", (request, response) => {
@@ -86,7 +116,7 @@ app.get("/contact", (request, response) => {
     });
 });
 
-// Get CONTACT
+// POST CONTACT
 app.post("/contact", function (request, response) {
     const email = request.body.email;
     const message = request.body.message;
@@ -99,38 +129,52 @@ app.post("/contact", function (request, response) {
     });
 });
 
-// Get NEWS
+// POST NEWS
 app.post("/news", function (request, response) {
     const date = request.body.date;
     const title = request.body.title;
-    // const image = request.body.image;
     const post = request.body.post;
+    // const image = request.body.image;
 
-    data.news.push({
-        id: data.news.at(-1).id + 1,
-        date: date,
-        title: title,
-        // image: image,
-        post: post,
+    const query = `INSERT INTO news (date, title, post) VALUES (?, ?, ?)`;
+    const values = [date, title, post];
+
+    database.run(query, values, function (error) {
+        response.redirect("/news");
     });
-
-    response.redirect("/news");
 });
 
-// Get EXPERIENCES
+// POST EXPERIENCES
 app.post("/experiences", function (request, response) {
     const name = request.body.name;
     const link = request.body.link;
     const experience = request.body.experience;
 
-    data.experiences.push({
-        id: data.experiences.at(-1).id + 1,
-        name: name,
-        link: link,
-        experience: experience,
-    });
+    const query = `INSERT INTO experiences (name, link, experience) VALUES (?, ?, ?)`;
+    const values = [name, link, experience];
 
-    response.redirect("/experiences");
+    database.run(query, values, function (error) {
+        response.redirect("/experiences");
+    });
+});
+
+// LOG IN
+app.post("/admin", function (request, response) {
+    const username = request.body.username;
+    const password = request.body.password;
+
+    if (username == adminUsername && password == adminPassword) {
+        request.session.isLoggedIn = true;
+        // document.getElementById("navigationAdmin").style.visibility = "visible";
+        // document.getElementById("navigationAdmin").style.height = "auto";
+        response.redirect("/");
+    } else {
+        response.render("admin", {
+            failedToLogin: true,
+            title: "Admin",
+            style: "admin.css",
+        });
+    }
 });
 
 app.listen(8080, () => {
